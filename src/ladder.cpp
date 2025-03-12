612667df252fb5766d3cfd35189d5fa2ee1291de
bryanctrl <bryatn12@uci.edu>
2025-03-12 07:16:58 +0000

    Implemented generate_word_ladder.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -55,7 +55,29 @@ bool is_adjacent(const string& word1, const string& word2){
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
-
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    set<string> visited;
+    visited.insert(begin_word);
+    while(!ladder_queue.empty()){
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+        for(auto w : word_list){
+            if (is_adjacent(last_word, w)){
+                if (visited.find(w) == visited.end()){
+                    visited.insert(w);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(w);
+                    if (w == end_word){
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return {};
 }
 
 void load_words(set<string> & word_list, const string& file_name){
